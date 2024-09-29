b6f046f73a2db6adbcd95ffc20fd02474ce8f970
Bulbul80SWE <sadia80@student.sust.edu>
2024-09-29 13:14:54 +0600

    loginInfo variable declaration

--- src/main/java/com/example/deena_stdnt/login/LoginInfo.java
+++ src/main/java/com/example/deena_stdnt/login/LoginInfo.java
@@ -1,6 +1,20 @@
 package com.example.deena_stdnt.login;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table (name="LOG IN")
+@Entity
 public class LoginInfo {
 
-
+    private String userName;
+    @Id
+    private String email;
+    private String password;
 }
