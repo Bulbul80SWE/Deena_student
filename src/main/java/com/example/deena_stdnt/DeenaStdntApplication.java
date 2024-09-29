3057b181c7709353896be9c2cfa6fdfc2df3e2c3
Bulbul80SWE <sadia80@student.sust.edu>
2024-09-29 11:34:42 +0600

    Initial Commit

---
+++ src/main/java/com/example/deena_stdnt/DeenaStdntApplication.java
@@ -0,0 +1,13 @@
+package com.example.deena_stdnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeenaStdntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeenaStdntApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/deena_stdnt/DeenaStdntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deena_stdnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeenaStdntApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
