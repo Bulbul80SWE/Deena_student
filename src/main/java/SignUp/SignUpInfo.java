7d0155f0f7cdb0e2a336da8ee5c8cd9b0361b92c
musfiqur24 <musfiqur.utsho@gmail.com>
2024-09-29 12:38:10 +0600

    Sign Up Model Created

--- src/main/java/SignUp/SignUpInfo.java
+++ src/main/java/SignUp/SignUpInfo.java
@@ -1,4 +1,24 @@
 package SignUp;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.lang.model.element.Name;
+
+@Getter
+@Setter
+@Table (name="SIGN UP")
+@Entity
 public class SignUpInfo {
+
+    private String email;
+    private String userName;
+    private String studentName;
+    private String phone;
+    private String rollNo;
+    private String password;
+    private String institue;
+    private String branch;
 }
